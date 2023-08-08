93cf3009326231d61caebd6659ff63968ca3cc61
alimirhashimli <alimirhashimli@gmail.com>
2023-08-08 03:49:05 +0200

    added jwt

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,6 +1,11 @@
 const mongoose = require("mongoose")
 const User = require("../models/userModel")
 
+const Schema = mongoose.Schema
+
+const createToken = (_id) => {
+    return jwt.sign({_id:_id}, process.env.SECRET, {expiresIn: "3d"})
+}
 
 // login user
 
@@ -16,7 +21,11 @@ const signupUser = async (req, res) => {
 
     try {
         const user = await User.signup(email, password)
-        res.status(200).json({email, user})
+        // create token
+        const token = createToken(user._id)
+
+
+        res.status(200).json({email, token})
     } catch (error) {
         res.status(400).json({error: error.message})
     }
