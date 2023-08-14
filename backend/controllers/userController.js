e3ed93aa554e5133fb90b6dd331a9e76cd488a21
alimirhashimli <alimirhashimli@gmail.com>
2023-08-14 02:07:31 +0200

    created login method

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose")
 const User = require("../models/userModel")
+const jwt = require("jsonwebtoken")
 
-const Schema = mongoose.Schema
 
 const createToken = (_id) => {
     return jwt.sign({_id:_id}, process.env.SECRET, {expiresIn: "3d"})
@@ -10,7 +10,16 @@ const createToken = (_id) => {
 // login user
 
 const loginUser = async (req, res) => {
-    res.json({msg: "login user"})
+    const {email, password} = req.body
+    
+
+    try {
+        const user = await User.login(email, password)
+        const token = createToken(user._id)
+        res.status(200).json({email, token})
+    } catch (error) {
+        res.status(400).json({error: error.message})
+    }
 }
 
 
--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,8 +1,8 @@
 const mongoose = require("mongoose")
 const bcrypt = require("bcrypt")
-const jwt = require("jsonwebtoken")
-
 const Schema = mongoose.Schema
+const validator = require("validator")
+
 
 const userSchema = new Schema({
     email: {
@@ -20,6 +20,18 @@ const userSchema = new Schema({
 // static signup method
 userSchema.statics.signup = async function(email, password) {
     
+    if(!email || !password){
+        throw Error("All fields must be filled")
+    }
+
+    if(!validator.isEmail(email)){
+        throw Error("Email not valid")
+    }
+
+    if(!validator.isStrongPassword(password)){
+        throw Error("Password not strong enough")
+    }
+
 
     const exists = await this.findOne({email})
 
@@ -34,6 +46,32 @@ userSchema.statics.signup = async function(email, password) {
 
     return user
 
+}
+
+
+// static login method
+userSchema.statics.login = async function(email, password)  {
+
+    if(!email || !password){
+        throw Error("All fields must be filled")
+    }
+
+
+
+
+    const user = await this.findOne({email})
+
+    if(!user){
+        throw Error("Incorrect email")
+    }
+
+    const match = await bcrypt.compare(password, user.password)
+
+    if(!match){
+        throw Error("Incorrect password")
+    }
+
+    return user
 }
 
 
