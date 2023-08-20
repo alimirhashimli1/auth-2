01adab889f181bdc44e2030d881c3e5b7105306b
alimirhashimli <alimirhashimli@gmail.com>
2023-08-20 03:45:33 +0200

    added the logout button

--- frontend/src/pages/Signup.js
+++ frontend/src/pages/Signup.js
@@ -1,54 +1,17 @@
 import React from 'react'
 import { useState } from 'react'
-import {useAuthContext} from "../hooks/useAuthContext";
-
+import { useSignup } from '../hooks/useSignup'
 
 const Signup = () => {
     const [email, setEmail] = useState()
     const [password, setPassword] = useState()
-    const [error, setError] = useState(null)
-    const [isLoading, setIsLoading] = useState(null)
-    const {dispatch} = useAuthContext()
-
-    const signup = async(email, password) => {
-      setIsLoading(true)
-      setError(null)
-      
-      const response = await fetch("/api/user/signup", {
-          method: "POST",
-          headers: {"Content-Type": "application/json"},
-      body: JSON.stringify({email, password})
-  })
-  const json = await response.json()
-
-
-  if(!response.ok){
-      setIsLoading(false)
-      setError(json.error)
-  }
-
-  if(response.ok){
-      // save the user to local storage
-      localStorage.setItem("user", JSON.stringify(json))
-
-      // update auth context
-      dispatch({type: "LOGIN", payload: json})
-
-      setIsLoading(false)
-  }
-}
-
-
-
-
+    const {signup, error, isLoading } = useSignup()
     
 
   const handleSubmit = async (e) => {
     e.preventDefault()
     await signup(email, password)
   }
-
-  
 
 
   return (
