1be11c9b8f53c39d370fbeac5bc94203e7ade73d
alimirhashimli <alimirhashimli@gmail.com>
2023-08-23 03:26:40 +0200

    added thre auth middleware

--- frontend/src/hooks/useAuthContext.js
+++ frontend/src/hooks/useAuthContext.js
@@ -1,13 +1,12 @@
-import {useContext} from "react"
-import { AuthContext } from "../context/AuthContext";
+import { AuthContext } from "../context/AuthContext"
+import { useContext } from "react"
 
 export const useAuthContext = () => {
-    const context = useContext(AuthContext)
+  const context = useContext(AuthContext)
 
+  if(!context) {
+    throw Error('useAuthContext must be used inside an AuthContextProvider')
+  }
 
-    if(!context){
-        throw Error("use AuthContext must be used inside a AuthContext provider")
-    }
-
-    return context
+  return context
 }
